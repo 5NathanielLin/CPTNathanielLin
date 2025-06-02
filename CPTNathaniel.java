64424badbf697259d41f6db129ffd85c55efe93f
5NathanielLin <nathaniel.lin26@ycdsbk12.ca>
2025-06-02 09:05:51 -0400

    Changed background color

--- CPTNathaniel.java
+++ CPTNathaniel.java
@@ -6,6 +6,11 @@ import java.awt.Font;
 public class CPTNathaniel{
 	public static void main(String[]args){
 		Console con = new Console(500,800);
+		// Changed Background to white 
+		con.setDrawColor(Color.WHITE);
+		con.fillRect(0,0,500,800);
+		
+		
 		
 		
 	}
