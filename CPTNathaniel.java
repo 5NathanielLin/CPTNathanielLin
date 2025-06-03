9c814a2213015f79f8f09305b1c5ae6a0893997f
5NathanielLin <nathaniel.lin26@ycdsbk12.ca>
2025-06-02 20:09:57 -0400

    Update CPTNathaniel.java

--- CPTNathaniel.java
+++ CPTNathaniel.java
@@ -5,13 +5,23 @@ import java.awt.Font;
 
 public class CPTNathaniel{
 	public static void main(String[]args){
-		Console con = new Console(500,800);
+		Console con = new Console();
+		//Font title = con.loadFont("", 40);
+		
 		// Changed Background to white 
 		con.setDrawColor(Color.WHITE);
-		con.fillRect(0,0,500,800);
+		con.fillRect(0,0,1000,1000);
 		
+		// Load title 
+		/*con.setDrawFont(title);
+		con.setDrawColor(new Color(0,0,0));
+		con.drawString("Math Training Game",0,0 );*/
 		
-		
+		// Buttons 
+		con.setDrawColor(new Color(0,0,0));
+		con.drawString("Play [P]", 425,300);
+		con.setDrawColor(new Color(0,0,0));
+		con.drawString("View Leaderboard [V]",425,325);
 		
 	}
 }
