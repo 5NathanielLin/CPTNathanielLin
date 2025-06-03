de4d01900db19f19a57989af6ea7f550db1079ae
5NathanielLin <nathaniel.lin26@ycdsbk12.ca>
2025-06-02 20:42:46 -0400

    Added all buttons and Logo

--- CPTNathaniel.java
+++ CPTNathaniel.java
@@ -5,23 +5,31 @@ import java.awt.Font;
 
 public class CPTNathaniel{
 	public static void main(String[]args){
-		Console con = new Console();
+		Console con = new Console(1200,1500);
+		BufferedImage imgMathLogo = con.loadImage("MathLogo.png");
 		//Font title = con.loadFont("", 40);
 		
 		// Changed Background to white 
 		con.setDrawColor(Color.WHITE);
-		con.fillRect(0,0,1000,1000);
+		con.fillRect(0,0,1200,1500);
 		
-		// Load title 
-		/*con.setDrawFont(title);
-		con.setDrawColor(new Color(0,0,0));
-		con.drawString("Math Training Game",0,0 );*/
+		//Logo
+		con.drawImage(imgMathLogo,300,75);
+
+		
 		
 		// Buttons 
 		con.setDrawColor(new Color(0,0,0));
-		con.drawString("Play [P]", 425,300);
+		con.drawString("Play [P]", 700,300);
 		con.setDrawColor(new Color(0,0,0));
-		con.drawString("View Leaderboard [V]",425,325);
+		con.drawString("View Leaderboard [V]",700,325);
+		con.setDrawColor(new Color(0,0,0));
+		con.drawString("Add Quiz [A]",700,350);
+		con.setDrawColor(new Color(0,0,0));
+		con.drawString("Quit [Q]",700,375);
+		
+		//Repainting to ensure above images are loading
+		con.repaint();
 		
 	}
 }
